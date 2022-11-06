021455b4d9121acc2c7fd6d938814531269e6f2b
Ajitomi Daisuke <daisuke.ajitomi@toshiba.co.jp>
2022-11-06 15:07:17 +0900

    Add dnt.ts.

---
+++ dnt.ts
@@ -0,0 +1,54 @@
+import { build, emptyDir } from "dnt";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  typeCheck: true,
+  test: true,
+  declaration: true,
+  scriptModule: "umd",
+  compilerOptions: {
+    lib: ["es2021", "dom"],
+  },
+  shims: {
+    deno: "dev",
+  },
+  package: {
+    name: "bhttp-js",
+    version: Deno.args[0],
+    description:
+      "A BHTTP (Binary Representation of HTTP Messages) Encoder and Decoder",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/dajiaji/bhttp-js.git",
+    },
+    homepage: "https://github.com/dajiaji/bhttp-js#readme",
+    license: "MIT",
+    main: "./script/mod.js",
+    types: "./types/mod.d.ts",
+    exports: {
+      ".": {
+        "import": "./esm/mod.js",
+        "require": "./script/mod.js",
+      },
+      "./package.json": "./package.json",
+    },
+    keywords: [
+      "bhttp",
+      "rfc9292",
+    ],
+    engines: {
+      "node": ">=16.0.0",
+    },
+    author: "Ajitomi Daisuke",
+    bugs: {
+      url: "https://github.com/dajiaji/bhttp-js/issues",
+    },
+  },
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
