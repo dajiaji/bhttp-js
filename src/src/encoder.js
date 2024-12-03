995fbcb7097828251041c5a6f2d402ee92e5713b
dajiaji <dajiaji@users.noreply.github.com>
2024-12-03 20:43:29 +0000

    deploy: 769a70b846950178c6ca460844edebc53b69c7e4

--- src/src/encoder.js
+++ src/src/encoder.js
@@ -50,8 +50,9 @@ class RequestEncoderContext extends EncoderContext {
         len += this.url.protocol.length - 1;
         len += this.calculateVliSize(this.url.host.length);
         len += this.url.host.length;
-        len += this.calculateVliSize(this.url.pathname.length);
+        len += this.calculateVliSize(this.url.pathname.length + this.url.search.length);
         len += this.url.pathname.length;
+        len += this.url.search.length;
         // Known Length Headers
         this.headerSize = 0;
         this.request.headers.forEach((value, key) => {
@@ -131,7 +132,7 @@ export class BHttpEncoder {
         this.encodeVliAndValue(ctx, ctx.request.method);
         this.encodeVliAndValue(ctx, ctx.url.protocol.slice(0, ctx.url.protocol.length - 1));
         this.encodeVliAndValue(ctx, ctx.url.host);
-        this.encodeVliAndValue(ctx, ctx.url.pathname);
+        this.encodeVliAndValue(ctx, ctx.url.pathname + ctx.url.search);
         // Known Length Headers
         this.encodeVli(ctx, ctx.headerSize);
         ctx.request.headers.forEach((value, key) => {
