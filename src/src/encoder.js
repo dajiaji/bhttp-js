518daaf5cef498615cc16ad46b46cbf64ff9d316
dajiaji <dajiaji@users.noreply.github.com>
2024-12-03 23:01:19 +0000

    deploy: ee5b0d0fb93dbcf21f683eea0f4616ac10cc5925

--- src/src/encoder.js
+++ src/src/encoder.js
@@ -50,15 +50,9 @@ class RequestEncoderContext extends EncoderContext {
         len += this.url.protocol.length - 1;
         len += this.calculateVliSize(this.url.host.length);
         len += this.url.host.length;
-        len += this.calculateVliSize(this.url.pathname.length + this.url.search.length +
-            ((this.url.search.length > 0 && !this.url.search.startsWith("?"))
-                ? 1
-                : 0));
+        len += this.calculateVliSize(this.url.pathname.length + this.url.search.length);
         len += this.url.pathname.length;
         len += this.url.search.length;
-        len += (this.url.search.length > 0 && !this.url.search.startsWith("?"))
-            ? 1
-            : 0;
         // Known Length Headers
         this.headerSize = 0;
         this.request.headers.forEach((value, key) => {
@@ -138,11 +132,7 @@ export class BHttpEncoder {
         this.encodeVliAndValue(ctx, ctx.request.method);
         this.encodeVliAndValue(ctx, ctx.url.protocol.slice(0, ctx.url.protocol.length - 1));
         this.encodeVliAndValue(ctx, ctx.url.host);
-        this.encodeVliAndValue(ctx, ctx.url.pathname +
-            (ctx.url.search.length > 0 && (!ctx.url.search.startsWith("?"))
-                ? "?"
-                : "") +
-            ctx.url.search);
+        this.encodeVliAndValue(ctx, ctx.url.pathname + ctx.url.search);
         // Known Length Headers
         this.encodeVli(ctx, ctx.headerSize);
         ctx.request.headers.forEach((value, key) => {
